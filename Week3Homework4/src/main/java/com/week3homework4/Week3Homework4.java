bb0ab89db773fa8df533a48e966959ff7346aee3
Muharrem AÇIKGÖZ <Muharrem AIKGZ@efe.home>
2022-11-10 14:29:57 +0300

    Week3 Homework4

---
+++ Week3Homework4/src/main/java/com/week3homework4/Week3Homework4.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.week3homework4;
+
+/**
+ *
+ * @author Muharrem AÇIKGÖZ
+ */
+public class Week3Homework4 {
+
+    public static void main(String[] args) {
+        String referenceType = "https://medium.com/@tr.muharremacikgoz/javada-de%C4%9Fer-ve-referans-tipler-545f49879209";
+        String interfaces = "https://medium.com/@tr.muharremacikgoz/javada-interface-aray%C3%BCz-1ea4e11c54fb";
+        System.out.println("Medium.com Ödevleri");
+        System.out.println("Değer ve Referans Tipler : " + referenceType);
+        System.out.println("İnterfaceler : " + interfaces);
+    }
+}
